cfea7093149d5b8a153853129f9926dc7c11b36f
David Rashid <davidmra@umich.edu>
2024-01-24 11:13:45 -0500

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#create-task-form').addEventListener('submit', (event) =>{
+    event.preventDefault()
+    addToList(event.target.elements['new-task-description'].value)
+  })
+})
+
+function addToList(todo){
+  let p = document.createElement('p');
+  p.textContent = todo;
+
+  let bttn = document.createElement('button');
+  bttn.textContent = 'X'
+  bttn.addEventListener('click', handleDelete);
+  function handleDelete(){
+    p.remove();
+  }
+  p.appendChild(bttn);
+  document.querySelector('#tasks').appendChild(p);
+}
